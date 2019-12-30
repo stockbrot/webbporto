bbf4de2fdc50ff00176eeb895eedb43c616b0656
orbit <webbtanis@gmail.com>
2019-12-30 18:46:18 +0100

    reverse the array

--- src/components/ImageGridComp.vue
+++ src/components/ImageGridComp.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap class="siteCardLayout">
-    <v-flex xs12 sm6 md4 px-3 v-for="col in columns" :key="col.id">
+    <v-flex xs12 sm6 md4 px-3 v-for="col in itemReverse" :key="col.id">
         <v-card
           class="mb-3"
           :style="{backgroundColor: randomColor(col.id)}"
@@ -13,7 +13,7 @@
               :style="`opacity: ${hover ? 0.4 : 1}`"
               class="siteImages elevation-10"
               @click.stop="col.dialog = true"
-              position="top center"
+              :position="col.img_pos"
             ></v-img>
           </v-hover>
           <v-container
@@ -51,13 +51,12 @@ export default {
           id: 0,
           dialog: false,
           img: 'https://cdnb.artstation.com/p/assets/images/images/022/100/895/large/tanis-webb-pre-render013-edited2.jpg?1574114343',
-          // img: require('../assets/images/sites/seo-free-seo-website-template.png'),
           title: 'Darth Vader\'s Saber',
+          img_pos: 'top left',
           dialogContent: {
             dialogText1: 'games.packageman.dialogText1',
             dialogText2: 'games.packageman.dialogText2',
-            img: 'https://cdnb.artstation.com/p/assets/images/images/022/100/895/large/tanis-webb-pre-render013-edited2.jpg?1574114343',
-            imgl: require('../assets/images/sites/seo-free-seo-website-template.png'),
+            img1: 'https://cdnb.artstation.com/p/assets/images/images/022/100/895/large/tanis-webb-pre-render013-edited2.jpg?1574114343',
             link: 'https://doctororbit.itch.io/package-man/'
           }
         },
@@ -66,22 +65,42 @@ export default {
           dialog: false,
           img: 'https://cdnb.artstation.com/p/assets/images/images/022/345/089/large/tanis-webb-overpaint.jpg?1575071367',
           title: 'The Mandalorian',
+          img_pos: 'top center',
           dialogContent: {
             dialogText1: 'games.packageman.dialogText1',
             dialogText2: 'games.packageman.dialogText2',
-            img: 'https://cdnb.artstation.com/p/assets/images/images/022/345/089/large/tanis-webb-overpaint.jpg?1575071367',
-            imgl: require('../assets/images/pm-dialog-lazy.png'),
+            img1: 'https://cdnb.artstation.com/p/assets/images/images/022/345/089/large/tanis-webb-overpaint.jpg?1575071367',
+            link: 'https://doctororbit.itch.io/package-man/'
+          }
+        },
+        {
+          id: 2,
+          dialog: false,
+          img: 'https://i.ytimg.com/vi/jHpvWiFybR8/sddefault.jpg',
+          title: 'Default Cube',
+          img_pos: 'center center',
+          dialogContent: {
+            html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jHpvWiFybR8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            dialogText2: 'games.packageman.dialogText2',
+            img1: 'https://cdnb.artstation.com/p/assets/images/images/022/345/089/large/tanis-webb-overpaint.jpg?1575071367',
             link: 'https://doctororbit.itch.io/package-man/'
           }
         }
+        // <iframe width="560" height="315" src="https://www.youtube.com/embed/jHpvWiFybR8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       ],
       colorCache: {}
     }
   },
   methods: {
     randomColor (id) {
-      const r = () => Math.floor(256 * Math.random());
+      const r = () => Math.floor(200 * Math.random() - 50);
       return this.colorCache[id] || (this.colorCache[id] = `rgb(${r()}, ${r()}, ${r()}) !important`);
+    }
+  },
+  computed: {
+    itemReverse() {
+      // eslint-disable-next-line
+      return this.columns.reverse()
     }
   }
 }
