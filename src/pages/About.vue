767e0cf9ce2a554fadbaf877109fcbd88d70193d
DoctorORBiT <taniswebb@gmail.com>
2020-01-01 03:18:23 +0100

    added about page

---
+++ src/pages/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout
+    row
+    wrap
+    class="sitePadding my-5"
+    align-center
+  >
+    <v-flex xs12 pa-5>
+      <v-card class="pa-4">
+        <h1 class="mb-3">{{ $t('about.header')}}</h1>
+        <br/>
+        <p>{{ $t('about.text1') }}</p>
+        <br>
+        <p>{{ $t('about.text2') }}</p>
+        <br>
+        <p>{{ $t('about.text3') }}</p>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      // data here and stuffs
+    }
+  }
+}
+</script>
+
+<style>
+</style>
