07e87fb2719290670a15f8ce68897630e856f988
DoctorORBiT <taniswebb@gmail.com>
2020-01-03 01:24:34 +0100

    wip

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,13 +7,13 @@
   >
     <v-flex xs12 pa-5>
       <v-card class="pa-4">
-        <h1 class="mb-3">{{ $t('about.header')}}</h1>
+        <h1 class="display-1 mb-3">{{ $t('about.header')}}</h1>
         <br/>
-        <p>{{ $t('about.text1') }}</p>
+        <p class="subheading">{{ $t('about.text1') }}</p>
         <br>
-        <p>{{ $t('about.text2') }}</p>
+        <p class="subheading">{{ $t('about.text2') }}</p>
         <br>
-        <p>{{ $t('about.text3') }}</p>
+        <p class="subheading">{{ $t('about.text3') }}</p>
       </v-card>
     </v-flex>
   </v-layout>
