2143cc1d1c4f7accab29e1462d6144bc201c3b4c
DoctorORBiT <taniswebb@gmail.com>
2020-01-03 01:23:37 +0100

    fixed responsiv

--- src/components/ParagraphComp.vue
+++ src/components/ParagraphComp.vue
@@ -1,19 +1,21 @@
 <template>
   <v-layout row wrap align-center class="mx-2 py-5">
-    <v-flex xs12 sm6 md4 v-if="isLeft">
+    <v-flex xs4 sm6 md4 v-if="isLeft" class="weird">
       <v-img
         :src="img"
         :lazy-src="imgl"
-        height="300"
+        max-height="300"
         contain
       />
     </v-flex>
     <v-flex xs12 sm6 md8>
-      <h4 class="display-1 mb-1" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ titel }}</h4>
-      <p class="subheading" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ textBlock }}</p>
-      <v-btn v-if="button" color="info" class="d-flex" width="150" :to="$i18nRoute({name: 'contact'})">{{ $t('contactMe') }}</v-btn>
+      <v-card class="pa-4">
+        <h1 class="display-1 mb-3 text-xs-caption" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ title }}</h1>
+        <p class="subheading" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ textBlock }}</p>
+        <v-btn v-if="button" color="info" class="d-flex" width="150" :to="button_to">{{ button_text }}</v-btn>
+      </v-card>
     </v-flex>
-    <v-flex xs12 sm6 md4 v-if="!isLeft">
+    <v-flex xs4 sm6 md4 v-if="!isLeft" class="weird">
       <v-img
         :src="img"
         :lazy-src="imgl"
@@ -28,15 +30,34 @@
 export default {
   name: 'ParagraphComp',
   props: {
-    titel: String,
+    title: String,
     textBlock: String,
     img: String,
     imgl: String,
     isLeft: Boolean,
-    button: Boolean
+    button: Boolean,
+    button_to: Object,
+    button_text: String
   }
 }
 </script>
 
-<style lang="stylus">
+<style>
+@media (max-width: 800px){
+  .v-image__image--contain {
+    background-size: 100px !important;
+  }
+  .parallaxComp {
+    background-size: cover !important;
+  }
+  .display-1 {
+    font-size: 25px !important;
+  }
+  .subheading {
+    font-size: 14px !important;
+  }
+  .weird {
+    display: none;
+  }
+}
 </style>
