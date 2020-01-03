35912c9274202e1ac50d72e758d88c3669cf0781
DoctorORBiT <taniswebb@gmail.com>
2020-01-03 01:24:17 +0100

    default language to EN

--- src/constants/trans.js
+++ src/constants/trans.js
@@ -1,6 +1,6 @@
 // Fallback language, incase something goes wrong
-export const DEFAULT_LANGUAGE = 'de'
-export const FALLBACK_LANGUAGE = 'de'
+export const DEFAULT_LANGUAGE = 'en'
+export const FALLBACK_LANGUAGE = 'en'
 
 // Add your supported languages here
 export const SUPPORTED_LANGUAGES = ['en', 'de']
