4e8af2edcb064ec02f5f3d33d4f55d2bbca7625f
DoctorORBiT <taniswebb@gmail.com>
2020-01-03 01:25:03 +0100

    fixed merge; moved paragraph to parallax

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,75 +5,34 @@
   >
     <v-layout justify-center>
       <v-flex>
-        <CarouselComp :items="items"/>
-        <!-- <ParallaxComp :para="false" img="/img/parallax.3a69e510.png"/> -->
-        <v-container fluid grid-list-md>
-          <ParagraphComp
-            :titel="$t('paragraph.titel')"
-            :textBlock="$t('paragraph.textBlock')"
-            :img="require('../assets/images/moi.png')"
-            :isLeft="true"
-          />
-        </v-container>
-          <ParallaxComp :para="true" img="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1920"/>
-        <v-container fluid grid-list-md>
-          <CardGridComp/>
-        </v-container>
-      </v-flex>
-    </v-layout>
-  </v-layout>
-</template>
-
-<script>
-import CardGridComp from '../components/CardGridComp'
-import ParagraphComp from '../components/ParagraphComp'
-import ParallaxComp from '../components/ParallaxComp'
-import CarouselComp from '../components/CarouselComp'
-
-export default {
-  components: {
-    CardGridComp,
-    ParagraphComp,
-    ParallaxComp,
-    CarouselComp
-  },
-  name: 'Home',
-  data () {
-    return {
-      items: [
-        { img: require('../assets/images/banner2.png'),
-          header: 'Game Development',
-          link: 'games' },
-        { img: require('../assets/images/banner1.png'),
-          header: 'Web Design',
-          link: 'sites' },
-        { img: require('../assets/images/banner3.png'),
-          header: 'Art Design',
-          link: 'design' },
-      ],
-    }
-  }
-}
-</script>
-
-<template>
-  <v-layout
-    column
-    align-center
-  >
-    <v-layout justify-center>
-      <v-flex>
         <!-- <CarouselComp :items="items"/> -->
-        <ParallaxComp :para="false" img="/img/parallax2.540f8d19.png"/>
-        <v-container fluid grid-list-md>
-          <ParagraphComp
-            :titel="$t('paragraph.titel')"
-            :textBlock="$t('paragraph.textBlock')"
-            :img="require('../assets/images/moi.png')"
-            :isLeft="true"
-          />
-        </v-container>
-          <ParallaxComp :para="true" img="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1920"/>
+        <ParallaxComp
+          :para="false"
+          :img="require('../assets/images/parallax2.png')"
+        />
+          <!-- style="background-color: #f6f6d3" -->
+        <ParallaxComp
+          :para="true"
+          :para_title="$t('paragraph.title')"
+          :para_text="$t('paragraph.textBlock')"
+          :para_img="require('../assets/images/moi.png')"
+          :para_isLeft="true"
+          :para_button="true"
+          :para_button_to="$i18nRoute({name: 'about'})"
+          :para_button_text="$t('more')"
+          img="https://cdnb.artstation.com/p/assets/images/images/022/100/895/large/tanis-webb-pre-render013-edited2.jpg?1574114343"
+        />
+          <!-- style="background-color: #333" -->
+        <ParallaxComp
+          :para="true"
+          :para_title="$t('paragraph2.title')"
+          :para_text="$t('paragraph2.textBlock')"
+          para_img="https://66.media.tumblr.com/6b7386c972fcd6cd7c9552936d83365c/tumblr_mpzhtdXxel1rvr9sko1_500.png"
+          :para_isLeft="false"
+          :para_button="false"
+          :img="require('../assets/images/saturn.jpg')"
+        />
+          <!-- style="background-color: #53515d" -->
         <v-container fluid grid-list-md>
           <CardGridComp/>
         </v-container>
@@ -84,14 +43,12 @@ export default {
 
 <script>
 import CardGridComp from '../components/CardGridComp'
-import ParagraphComp from '../components/ParagraphComp'
 import ParallaxComp from '../components/ParallaxComp'
 // import CarouselComp from '../components/CarouselComp'
 
 export default {
   components: {
     CardGridComp,
-    ParagraphComp,
     ParallaxComp
     // CarouselComp
   },
