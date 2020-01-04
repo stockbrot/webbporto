030732464bf8e27d2ff1fb87c104feeee9cf4d4c
doctororbit <taniswebb@gmail.com>
2020-01-04 02:01:54 +0100

    fixed spacing

--- src/components/ParagraphComp.vue
+++ src/components/ParagraphComp.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-layout row wrap align-center class="mx-2 py-5">
-    <v-flex xs4 sm6 md4 v-if="isLeft" class="weird">
+  <v-layout row wrap align-center justify-center class="mx-2 py-5">
+    <v-flex xs4 sm5 md3 v-if="isLeft" class="weird">
       <v-img
         :src="img"
         :lazy-src="imgl"
         max-height="300"
         contain
       />
+      <!-- <v-spacer/> -->
     </v-flex>
-    <v-flex xs12 sm6 md8>
+    <v-flex xs12 sm6 md7>
       <v-card class="pa-4">
         <h1 class="display-1 mb-3 text-xs-caption" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ title }}</h1>
         <p class="subheading" :class="`text-xs-${isLeft ? 'left' : 'right'}`">{{ textBlock }}</p>
         <v-btn v-if="button" color="info" class="d-flex" width="150" :to="button_to">{{ button_text }}</v-btn>
       </v-card>
     </v-flex>
-    <v-flex xs4 sm6 md4 v-if="!isLeft" class="weird">
+    <v-flex xs4 sm5 md3 v-if="!isLeft" class="weird">
       <v-img
         :src="img"
         :lazy-src="imgl"
-        height="300"
+        max-height="300"
         contain
       />
+      <!-- <v-spacer/> -->
     </v-flex>
   </v-layout>
 </template>
@@ -44,12 +46,6 @@ export default {
 
 <style>
 @media (max-width: 800px){
-  .v-image__image--contain {
-    background-size: 100px !important;
-  }
-  .parallaxComp {
-    background-size: cover !important;
-  }
   .display-1 {
     font-size: 25px !important;
   }
