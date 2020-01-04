ec767a96d969b736324837402d8e8c46574c1b17
doctororbit <taniswebb@gmail.com>
2020-01-04 02:02:53 +0100

    set default lang to en

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,11 +1,11 @@
 import VueI18n from 'vue-i18n'
 import Vue from 'vue'
 import { DEFAULT_LANGUAGE, FALLBACK_LANGUAGE } from '@/constants/trans'
-import de from '@/lang/de.json'
+import en from '@/lang/en.json'
 
 Vue.use(VueI18n)
 export const i18n = new VueI18n({
   locale: DEFAULT_LANGUAGE, // set locale
   fallbackLocale: FALLBACK_LANGUAGE,
-  messages: { de }// set locale messages
+  messages: { en }// set locale messages
 })
