293a0e1f79043ec418d1c135c9c33ce512b51265
doctororbit <taniswebb@gmail.com>
2020-01-04 02:02:39 +0100

    removed carouselcomp

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,12 +5,11 @@
   >
     <v-layout justify-center>
       <v-flex>
-        <!-- <CarouselComp :items="items"/> -->
         <ParallaxComp
           :para="false"
           :img="require('../assets/images/parallax2.png')"
+          :img2="require('../assets/images/parallax2_low.png')"
         />
-          <!-- style="background-color: #f6f6d3" -->
         <ParallaxComp
           :para="true"
           :para_title="$t('paragraph.title')"
@@ -21,18 +20,18 @@
           :para_button_to="$i18nRoute({name: 'about'})"
           :para_button_text="$t('more')"
           img="https://cdnb.artstation.com/p/assets/images/images/022/100/895/large/tanis-webb-pre-render013-edited2.jpg?1574114343"
+          :img2="require('../assets/images/saber_low.png')"
         />
-          <!-- style="background-color: #333" -->
         <ParallaxComp
           :para="true"
           :para_title="$t('paragraph2.title')"
           :para_text="$t('paragraph2.textBlock')"
-          para_img="https://66.media.tumblr.com/6b7386c972fcd6cd7c9552936d83365c/tumblr_mpzhtdXxel1rvr9sko1_500.png"
+          :para_img="require('../assets/images/moi.png')"
           :para_isLeft="false"
           :para_button="false"
-          :img="require('../assets/images/saturn.jpg')"
+          :img="require('../assets/images/saturn.png')"
+          :img2="require('../assets/images/saturn_low.png')"
         />
-          <!-- style="background-color: #53515d" -->
         <v-container fluid grid-list-md>
           <CardGridComp/>
         </v-container>
@@ -44,28 +43,26 @@
 <script>
 import CardGridComp from '../components/CardGridComp'
 import ParallaxComp from '../components/ParallaxComp'
-// import CarouselComp from '../components/CarouselComp'
 
 export default {
   components: {
     CardGridComp,
     ParallaxComp
-    // CarouselComp
   },
   name: 'Home',
   data () {
     return {
-      items: [
-        { img: require('../assets/images/banner2.png'),
-          header: 'Game Development',
-          link: 'games' },
-        // { img: require('../assets/images/banner1.png'),
-        //   header: 'Web Design',
-        //   link: 'sites' },
-        { img: require('../assets/images/banner3.png'),
-          header: 'Art Design',
-          link: 'design' },
-      ],
+      // items: [
+      //   { img: require('../assets/images/banner2.png'),
+      //     header: 'Game Development',
+      //     link: 'games' },
+      // { img: require('../assets/images/banner1.png'),
+      //   header: 'Web Design',
+      //   link: 'sites' },
+      //   { img: require('../assets/images/banner3.png'),
+      //     header: 'Art Design',
+      //     link: 'design' },
+      // ],
     }
   }
 }
