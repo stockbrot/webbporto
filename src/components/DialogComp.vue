0e3884542e8a317f526512305c3498f20e2fd273
doctororbit <taniswebb@gmail.com>
2020-01-07 02:08:24 +0100

    added dialog for images

--- src/components/DialogComp.vue
+++ src/components/DialogComp.vue
@@ -12,30 +12,66 @@
             pa-2
           >
             <v-layout align-center justify-center row wrap>
-              <v-flex class="xs12 sm6 md8">
-                <div class="py-3">
+              <v-flex class="xs12">
+                <div
+                  v-if="post.dialogContent.html"
+                  v-html="post.dialogContent.html">
+                </div>
+                  <!-- @click.stop="post.imageContent.dialog = true" -->
+                <v-btn
+                  icon
+                  dark
+                  @click="post.dialog = false"
+                >
+                  <v-icon>fas fa-times-circle</v-icon>
+                </v-btn>
+                <v-img
+                  @click="post.images.main1.dialog = !post.images.main1.dialog"
+                  :src="post.img"
+                  :lazy-src="post.imgl"
+                  class="mx-a"
+                  v-if="!post.dialogContent.html"
+                  max-height="600px"
+                  contain
+                >
+                  <template v-slot:placeholder>
+                    <v-layout
+                      align-center
+                      justify-center
+                      class="fill-height"
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
+              </v-flex>
+              <v-flex class="xs8">
+                <div class="py-5">
                   <h2 class="pb-3 text-xs-center">{{ post.title }}</h2>
                 <div class="px-4 text-xs-left">{{ $t(post.dialogContent.dialogText1) }}</div><br/>
-                <div v-if="post.dialogContent.html" v-html="post.dialogContent.html"></div>
                 </div>
-              </v-flex>
-              <v-flex class="xs12 sm6 md4">
-                <v-img
-                  :src="post.dialogContent.img1"
-                  :lazy-src="lazy_img"
-                  class="mx-a"
-                ></v-img>
               </v-flex>
             </v-layout>
             <!-- 2nd part -->
             <!-- v-if="post.title2" -->
             <v-layout align-center justify-center row wrap>
-              <v-flex class="xs12 sm6 md4">
+              <v-flex class="xs12 sm6 md3">
                 <v-img
-                  :src="post.dialogContent.img2"
-                  :lazy-src="lazy_img"
+                  @click="post.images.main2.dialog = !post.images.main2.dialog"
+                  :src="post.img"
+                  :lazy-src="post.imgl"
                   class="mx-a"
-                ></v-img>
+                >
+                  <template v-slot:placeholder>
+                    <v-layout
+                      align-center
+                      justify-center
+                      class="fill-height"
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
               </v-flex>
               <v-flex class="xs12 sm6 md8">
                 <div class="py-3">
@@ -57,11 +93,37 @@
           </v-container>
         </v-card>
       </v-dialog>
+      <template v-for="imgs in post.images">
+        <v-dialog
+          v-model="imgs.dialog"
+          fullscreen
+          :key="imgs.key"
+        >
+          <v-img
+            fullscreen
+            transition="dialog-bottom-transition"
+            :src="imgs.img"
+            :lazy-src="imgs.imgl"
+            contain
+            width="100vw"
+            height="100vh"
+          >
+            <v-btn
+              icon
+              dark
+              @click="imgs.dialog = false"
+            >
+              <v-icon>fas fa-times-circle</v-icon>
+            </v-btn>
+          </v-img>
+        </v-dialog>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'DialogComp',
   props: {
@@ -70,7 +132,6 @@ export default {
   },
   data () {
     return {
-      lazy_img: require('../assets/images/lazy/placeholder.jpeg'),
     }
   }
 }
