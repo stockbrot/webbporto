798e09c61fd20bf9b12cf769b0df4f886b9eaf4c
doctororbit <taniswebb@gmail.com>
2020-01-07 02:10:21 +0100

    page transitions

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,9 @@
     <v-content>
       <ToolbarComp siteTitle="Tanis Webb"/>
 
-      <router-view class="routerclass"/>
+      <transition name="fade" mode="out-in">
+        <router-view class="routerclass view"/>
+      </transition>
     </v-content>
   </div>
 </template>
@@ -19,11 +21,21 @@ export default {
   data () {
     return {
     }
-  },
+  }
 }
 </script>
 
 <style lang="stylus">
 .routerclass
   padding-top 50px
+
+.fade-enter-active,
+.fade-leave-active
+  transition-duration 0.2s
+  transition-property opacity
+  transition-timing-function ease
+
+.fade-enter,
+.fade-leave-active
+  opacity 0
 </style>
